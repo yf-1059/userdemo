f0022ed92261c528680b6a6d93d6754dee8b2817
yf-1059 <yufeiteoh@student.usm.my>
2023-09-08 23:41:28 +0800

    Remove unnecessary import

--- backend/src/main/java/com/example/userdemo/UserDemoApplication.java
+++ backend/src/main/java/com/example/userdemo/UserDemoApplication.java
@@ -1,14 +1,7 @@
 package com.example.userdemo;
 
-import java.util.stream.Stream;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.example.userdemo.model.User;
-import com.example.userdemo.repository.UserRepository;
 
 @SpringBootApplication
 public class UserDemoApplication {
